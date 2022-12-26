8c715761ece282ae717419b665bfec9d19f4e260
Shivam Yadav <shivamyadav93000@gmail.com>
2022-12-26 17:45:15 +0530

    add

---
+++ new/checkvowelOrConsonent.js
@@ -0,0 +1,5 @@
+function 
+step 1 = store value of input in a variable,
+step 2 = Check if condition for the letter is vowel or not,
+step 3 = if it is a vowel so print ("it is vowel")
+step 4 = or if it is not a vowel so  in else condition print ("it is a consonent")
